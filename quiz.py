b70e70d2ee0d2742e28cb43da6bc23fa83406226
Sachin <sachinduhan1223@gmail.com>
2022-12-26 10:21:18 +0530

    final_commit

--- quiz.py
+++ quiz.py
@@ -1,71 +1,98 @@
-import time
-import random
+# CONSTANTS
+TOTAL_QUESTION = 5
+SCORE = 0
 
-
-def show_instructions(instructions: list):
-    for instruction in instructions:
-        time.sleep(2)
-        print(instruction)
-
-
-def timer():
-    """
-    Show time left for quiz => Default: 60sec
-    """
-    time_in_secs = 10
-    while time_in_secs != 0:
-        # mins, secs = divmod(time_in_secs, 60)
-        # timer = "{:02d}:{:02d}".format(mins, secs)
-        timer = time_in_secs
-        print(f"Time left: {timer}", end="\r")
-        time_in_secs -= 1
-        time.sleep(1)
-    print("Time is up!")
-
-
-timer()
-
-# show_instructions(
-#     [
-#         " \n -------------------------\n Welcome to quiz !! \n -------------------------",
-#         "You will be asked 5 questions.",
-#         "You will be given 2 choices either True/False",
-#         "You will be given 20 points for each correct answer.",
-#         "GOOD LUCK!",
-#         "\n",
-#     ]
-# )
-
-questions = {
-    ">> Delhi is Capital of India": "true",
-    ">> Steve Jobs is the founder of Microsoft": "false",
-    ">> Jack Dorsey is the founder of Twitter": "true",
-    ">> Rust is a programming language": "true",
-    ">> Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson.": "true",
-    ">> JavaScript is most used programming language for web": "true",
-    ">> Python is most used programming language for AI and Machine Learnign": "true",
+QUESTIONS = {
+    "In school, Albert Einstein failed most of the subjects, except for physics and math.": "y",
+    "The first song ever sung in the space was Happy Birthday.": "y",
+    " A male canary tends to have a better singing voice than a female canary.": "y",
+    "Tea contains more caffeine than coffee and soda": "n",
+    "Dogs have an appendix.": "n",
+    "Bill Gates is the founder of Amazon.": "n",
+    "Mice have more bones than humans.": "y",
+    "The first product with a bar code was chewing gum": "y",
+    "The Beatles is a famous rock band from Manchester, the United Kingdom": "n",
+    "The star is the most common symbol used in all national flags around the world.": "y",
 }
 
+# IMPORTS
+import random, time, sys
 
-def start_quiz():
-    score = 0
-    for question, answer in questions.items():
-        user_answer = input(f"{question} (True/False): ").lower()
-        #
-        if user_answer == str(answer) or str(
-            answer[0]
-        ):  # show correct even if user enter first letter of answer (t/f)
-            print("Correct!")
-            score += 20
-        else:
-            print("Incorrect!")
+
+def welcome_and_greets():
+    """
+    Returns welcome message and greets user
+    """
+    print("Welcome to Quiz game")
     print(
-        f" \n -------------------------\n Your score is {score} \n -------------------------"
+        """
+    
+░██████╗░██╗░░░██╗██╗███████╗  ░██████╗░░█████╗░███╗░░░███╗███████╗
+██╔═══██╗██║░░░██║██║╚════██║  ██╔════╝░██╔══██╗████╗░████║██╔════╝
+██║██╗██║██║░░░██║██║░░███╔═╝  ██║░░██╗░███████║██╔████╔██║█████╗░░
+╚██████╔╝██║░░░██║██║██╔══╝░░  ██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░
+░╚═██╔═╝░╚██████╔╝██║███████╗  ╚██████╔╝██║░░██║██║░╚═╝░██║███████╗
+░░░╚═╝░░░░╚═════╝░╚═╝╚══════╝  ░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝
+
+
+█ █▄░█ █▀ ▀█▀ █▀█ █░█ █▀▀ ▀█▀ █ █▀█ █▄░█ █▀
+█ █░▀█ ▄█ ░█░ █▀▄ █▄█ █▄▄ ░█░ █ █▄█ █░▀█ ▄█
+
+    1. Qᴜɪᴢ ɢᴀᴍᴇ ʜᴀs 5 ᴏ̨ᴜᴇsᴛɪᴏɴs.
+    2. Each questions is of 2 points.
+    3. ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴄʜᴏᴏsᴇ ᴇɪᴛʜᴇʀ (y/n)
+    4. PRESS E/e ᴛᴏ ᴇxɪᴛ
+    """
     )
 
 
-if __name__ == "__main__":
-    start_quiz()
+welcome_and_greets()
+
+# Ascii art generated from => https://fsymbols.com/generators/carty/
 
 
-# TODO: Make use of threading to run timer function in background with start_quiz()
+randomQuestions = random.sample(
+    QUESTIONS.items(), TOTAL_QUESTION
+)  # Get 5 random questions
+
+correctAnswerMessages = [
+    "Congrats! You got it right ✅",
+    "Congatulations, It is correct 🎉",
+    "Thats right, great 😉",
+]  # Add more if you want
+
+incorrectAnswerMessages = ["Not correct ❌", "Oops, Wrong answer 😔", "Not right 😕"]
+
+run = "y"
+
+while run[0].lower() == "y":
+
+    for question, answer in randomQuestions:
+        print(f">> {question} (Y/N)")
+        userAnswer = input("Enter answer: ")
+
+        if userAnswer == "E" or userAnswer == "e":
+            print("Exiting...")
+            time.sleep(0.5)
+            print(f"Your score is {SCORE} out of {TOTAL_QUESTION * 2}")
+            sys.exit(0)  # exit the program if user enters E or e
+
+        if answer == userAnswer.lower():
+            print(
+                random.choice(correctAnswerMessages) + "\n"
+            )  # show random message from list
+            SCORE += 2
+        else:
+            print(random.choice(incorrectAnswerMessages) + "\n")
+
+    print(
+        f"""    -------------------------------------------------------------------------
+        Your score is {SCORE} out of {TOTAL_QUESTION * 2} and you got {int(SCORE / 2)} answers correct
+        ----------------------------------------------------------------------------------"""
+    )
+    print("\n")
+
+    run = input("Do you want to play again? (y/n): ")
+    print("\n")
+
+print("THANKS FOR PLAYING!!!")
